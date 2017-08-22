f3e3810ec2f081dd32031df6d148ad646608c175
KwatME <kwat.medetgul.ernar@gmail.com>
2017-08-22 13:33:06 -0700

    Use 2 branches to test

--- notebooks/environment.py
+++ notebooks/environment.py
@@ -3,10 +3,14 @@ sys.path.insert(0, '../tools/')
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-import ccal
+from file.file.gct import read_gct, write_gct
+from match.match.match_panel import make_match_panel, make_summary_match_panel
+from helper.helper.df import get_top_and_bottom_indices
+from plot.plot.plot import plot_heatmap
+from plot.plot.plot_nmf import plot_nmf
+from oncogps.oncogps.oncogps import define_components
+from match.match.comparison_panel import make_comparison_panel
 
 blue = '#4292C6'
 green = '#41AB5D'
