e399ce2a6b27395a3e792a06c52ede495c80cd7f
KwatME <kwatme8@gmail.com>
2017-10-06 19:57:52 -0700

    save

--- notebooks/notebook_environment.py
+++ notebooks/notebook_environment.py
@@ -13,14 +13,7 @@ import seaborn as sns
 
 sys.path.insert(0, '../tools/')
 print('Added \'../tools/\' to the path.')
-
-from repositories.helper.helper import df
-from repositories.helper.helper import file
-from repositories.file.file import gct
-from repositories.plot.plot import plot
-from repositories.plot.plot import plot_nmf
-from repositories.match.match import match_panel, comparison_panel
-from repositories.oncogps.oncogps import oncogps
+from ccal import ccal
 
 BLACK = '#000000'
 WHITE = '#FFFFFF' 
