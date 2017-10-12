9c0deb07f3bfe7f0de76318fddc8f9b210c71e0d
KwatME <kwatme8@gmail.com>
2017-10-11 17:37:45 -0700

    Refactored

--- notebooks/notebook_environment.py
+++ notebooks/notebook_environment.py
@@ -1,24 +1,18 @@
 import copy
+import gzip
 import itertools
 import os
 import pickle
 import re
 import sys
+from collections import OrderedDict
 from pprint import pprint
 
+import matplotlib as mpl
 import numpy as np
 import pandas as pd
-import matplotlib as mpl
 import seaborn as sns
 
-sys.path.insert(0, '../tools/')
-print('Added \'../tools/\' to the path.')
+sys.path.insert(0, '../tools')
+print('Added \'../tools\' to the path.')
 from ccal import ccal
-
-BLACK = '#000000'
-WHITE = '#FFFFFF' 
-PURPLE = '#807DBA'
-BLUE = '#4292C6'
-LIGHT_BLUE = '#0099CC'
-GREEN = '#41AB5D'
-RED = '#EF3B2C'
