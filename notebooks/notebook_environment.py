4fc1dab3048b64fab358daf7199334efc3df9a58
Kazyra <kazyra@ocean.home>
2017-10-15 00:21:00 -0700

    updated notebooks, added media,and added data you cant get via notebook 1

--- notebooks/notebook_environment.py
+++ notebooks/notebook_environment.py
@@ -13,6 +13,9 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+import warnings
+warnings.filterwarnings('ignore')
+
 sys.path.insert(0, '../tools')
 print('Added \'../tools\' to the path.')
 from ccal import ccal
