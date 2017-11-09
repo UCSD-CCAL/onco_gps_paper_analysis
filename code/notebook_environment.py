4a40ac2ae2ebf3848a79f86077174f5e00323214
KwatME <kwatme8@gmail.com>
2017-11-09 02:35:18 -0800

    Removed sys.path.insert

--- code/notebook_environment.py
+++ code/notebook_environment.py
@@ -15,6 +15,4 @@ import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
 
-sys.path.insert(0, '/Users/k/Jumis/github_kwatme')
-print('Added \'../tools\' to the path.')
 from ccal import ccal
